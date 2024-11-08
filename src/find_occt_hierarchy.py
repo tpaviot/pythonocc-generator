b038aaeb47ddce128e9e681372a753d48fb95754
Thomas Paviot <tpaviot@gmail.com>
2024-11-08 04:50:48 +0100

    OCCT structure to json

--- src/find_occt_hierarchy.py
+++ src/find_occt_hierarchy.py
@@ -1,9 +1,24 @@
+import json
 import os
 import glob
 
-OCCT_DIRECTORY = "/home/thomas/Téléchargements/OCCT-7_8_1_ii/" 
+OCCT_DIRECTORY = "/home/thomas/Téléchargements/OCCT-7_8_1_ii/"
 OCCT_SRC_PATH = os.path.join(OCCT_DIRECTORY, "src")
 
 # load all TK folders
-all_toolkits = glob.glob(OCCT_SRC_PATH, "TK*")
-print(all_toolkits)
+all_toolkits = glob.glob(os.path.join(OCCT_SRC_PATH, "TK*"))
+
+toolkits = {}
+# loop over toolkits
+for toolkit in all_toolkits:
+    toolkit_name = toolkit.split(os.sep)[-1]
+    with open(os.path.join(toolkit, "PACKAGES"), "r") as f:
+        packages = [l.strip() for l in f.readlines()]
+    # alphabetical sort
+    packages.sort()
+    # add the entry to the dict
+    toolkits[toolkit_name] = packages
+
+# save to json
+with open("toolkits.json", "w") as f:
+    f.write(json.dumps(toolkits, indent=4, ensure_ascii=True))
